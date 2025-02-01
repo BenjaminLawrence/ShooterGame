ca69eca277bfeba03934abe3787e2dbd3922b0f4
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-01 18:30:10 +0800

    Add reset to Level script and delta time to Logo script

--- scenes/Logo.gd
+++ scenes/Logo.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 var pos: Vector2 = Vector2.ZERO
 var test_scale: int = 1
-const speed: int = 10
+const speed: int = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,7 +18,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pos.x += speed
+	pos.x += speed * delta
 	position = pos
 	
 	#test_scale += 1
--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
+var test_array: Array[String] = ["Test", "Hello", "stuff"]
+
 func _ready() -> void:
-	print('level node is ready')
+	$Logo.rotation_degrees = 90
+	
+	for i in test_array:
+		print(i)
+
+func _process(delta: float) -> void:
+	$Logo.rotation_degrees += 5
+	
+	if $Logo.position.x > 1000:
+		$Logo.pos.x = 0
