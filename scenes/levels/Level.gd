9b2a43eb74b924926f125f7a8a60b570dba50539
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-02 17:13:45 +0800

    Clear empty line

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -4,9 +4,7 @@ var test_array: Array[String] = ["Test", "Hello", "stuff"]
 
 func _ready() -> void:
 	$Logo.rotation_degrees = 90
-	
-	for i in test_array:
-		print(i)
+
 
 func _process(delta: float) -> void:
 	$Logo.rotation_degrees += 200 * delta
