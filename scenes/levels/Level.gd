cf20376492505ba51d01b0239d5bbb2462f4f4e8
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-02 17:46:57 +0800

    Remove references to logo

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -1,13 +1,3 @@
 extends Node2D
 
 var test_array: Array[String] = ["Test", "Hello", "stuff"]
-
-func _ready() -> void:
-	$Logo.rotation_degrees = 90
-
-
-func _process(delta: float) -> void:
-	$Logo.rotation_degrees += 200 * delta
-	
-	if $Logo.position.x > 1000:
-		$Logo.pos.x = 0
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * 500
