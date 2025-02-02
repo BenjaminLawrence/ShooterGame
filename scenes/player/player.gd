d83c2ff521ac3c1ba00bd883029c53491abedede
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-02 17:02:35 +0800

    Add movement to player and also shooting template

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	
+	var direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * 500 * delta
 	
+	if Input.is_action_pressed("primary action"):
+		print("shoot laser")
