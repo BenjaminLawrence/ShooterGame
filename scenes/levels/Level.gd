833d61bfd3b442f5ed79733e7e37d1e257337f1e
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-08 15:38:49 +0800

    Add follow camera

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -3,3 +3,11 @@ extends Node2D
 
 func _on_gate_player_entered_gate() -> void:
 	print("player has entered gate")
+
+
+func _on_player_shoot_laser() -> void:
+	print("laser from level")
+
+
+func _on_player_throw_grenade() -> void:
+	print("grenade from level")
