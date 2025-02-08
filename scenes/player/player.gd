af9478b4b91aeb522a5655eec936cb43bf4d3329
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-08 15:03:58 +0800

    Add laser and grenade cooldown

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,13 +1,28 @@
 extends CharacterBody2D
 
+var can_laser: bool = true
+var can_grenade: bool = true
+
 func _process(_delta: float) -> void:
 	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * 500
 	move_and_slide()
 	
-	if Input.is_action_pressed("primary action"):
+	if Input.is_action_pressed("primary action") and can_laser:
 		print("shoot laser")
+		can_laser = false
+		$LaserTimer.start()
 	
-	if Input.is_action_pressed("secondary action"):
-		print("shoot grenade")
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		print("throw grenade")
+		can_grenade = false
+		$GrenadeTimer.start()
+
+
+func _on_laser_timer_timeout() -> void:
+	can_laser = true
+
+
+func _on_grenade_timer_timeout() -> void:
+	can_grenade = true
