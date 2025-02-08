5ba64bc62d6544463922d69657fc5bc3b7841d50
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-08 16:33:44 +0800

    Clean up projectile start positions

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -12,16 +12,17 @@ func _process(_delta: float) -> void:
 	move_and_slide()
 	
 	if Input.is_action_pressed("primary action") and can_laser:
-		var laser_markers = $LaserStartPositions.get_children()
-		var selected_laser = laser_markers[randi() % laser_markers.size()]
+		var laser_markers = $ProjectileStartPositions.get_children()
+		var selected_marker = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$LaserTimer.start()
-		shoot_laser.emit(selected_laser.global_position)
+		shoot_laser.emit(selected_marker.global_position)
 	
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		can_grenade = false
 		$GrenadeTimer.start()
-		throw_grenade.emit($GrenadeStartPosition.global_position)
+		var pos_marker = $ProjectileStartPositions.get_children()[0]
+		throw_grenade.emit(pos_marker.global_position)
 
 
 func _on_laser_timer_timeout() -> void:
