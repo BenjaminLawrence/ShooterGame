b7c1c066f28a7a6d3a369a93e85ffc9e71608aa1
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-08 16:11:25 +0800

    Add laser movement and clean up laser spawning

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -6,10 +6,10 @@ func _on_gate_player_entered_gate() -> void:
 	print("player has entered gate")
 
 
-func _on_player_shoot_laser(position) -> void:
+func _on_player_shoot_laser(pos) -> void:
 	var laser = laser_scene.instantiate()
-	laser.position = position
-	add_child(laser)
+	laser.position = pos
+	$Projectiles.add_child(laser)
 
 
 func _on_player_throw_grenade() -> void:
