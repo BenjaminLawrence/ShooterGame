a402a8fcc53a3efd92bae62e1c8de24e3f59cd19
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-13 22:36:59 +0800

    Update day/night cycle to use tweens and remove the transition timer

--- scenes/levels/day_night_cycle.gd
+++ scenes/levels/day_night_cycle.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 var is_day: bool = true
 var is_night: bool = false
-var is_sun_rising: bool = false
-var is_sun_setting: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,32 +11,29 @@ func _ready() -> void:
 func _on_cycle_timer_timeout() -> void:
 	if is_day:
 		is_day = false
-		is_sun_setting = true
-		$TransitionTimer.start()
-	elif is_night:
+		_sun_set()
+	else:
 		is_night = false
-		is_sun_rising = true
-		$TransitionTimer.start()
+		_sun_rise()
 
 
-func _on_transition_timer_timeout() -> void:
-	if is_sun_setting:
-		if $DayLight.energy > 0:
-			$DayLight.energy -= 0.01
-		elif $NightLight.energy < 1:
-			$NightLight.energy += 0.01
-		else:
-			is_sun_setting = false
-			is_night = true
-			$TransitionTimer.stop()
-			$CycleTimer.start()
-	elif is_sun_rising:
-		if $NightLight.energy > 0:
-			$NightLight.energy -= 0.01
-		elif $DayLight.energy < 1:
-			$DayLight.energy += 0.01
-		else:
-			is_sun_rising = false
-			is_day = true
-			$TransitionTimer.stop()
-			$CycleTimer.start()
+func _sun_set() -> void:
+	var day_tween = create_tween()
+	day_tween.tween_property($DayLight, "energy", 0, 10)
+	await day_tween.finished
+	var night_tween = create_tween()
+	night_tween.tween_property($NightLight, "energy", 1, 10)
+	await night_tween.finished
+	is_night = true
+	$CycleTimer.start()
+
+
+func _sun_rise() -> void:
+	var night_tween = create_tween()
+	night_tween.tween_property($NightLight, "energy", 0, 10)
+	await night_tween.finished
+	var day_tween = create_tween()
+	day_tween.tween_property($DayLight, "energy", 1, 10)
+	await day_tween.finished
+	is_day = true
+	$CycleTimer.start()
