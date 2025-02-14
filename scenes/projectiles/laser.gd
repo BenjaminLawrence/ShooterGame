45617b879798bc6b65ee03016954c568fce3f9d1
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-14 10:32:45 +0800

    Update laser to start timer in ready function instead of autostart

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -3,6 +3,10 @@ extends Area2D
 @export var speed: int = 1000
 var direction: Vector2 = Vector2.UP
 
+func _ready() -> void:
+	$LifespanTimer.start()
+
+
 func _process(delta: float) -> void:
 	position += direction * speed * delta
 
