f8213ed3d7fdee3e622ddf5f42902823af058ccb
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-14 13:28:19 +0800

    Fix method call typo

--- scenes/objects/emergency_light.gd
+++ scenes/objects/emergency_light.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.start("Pulsing")
+	$AnimationPlayer.play("Pulsing")
 
 
 func shutdown() -> void:
