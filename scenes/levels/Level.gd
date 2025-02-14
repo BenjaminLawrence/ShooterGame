bf705a7ccffc867fd02ee7a07a45dfe1f0221a4e
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-02-14 11:41:15 +0800

    Move outside triggers from level scene to outside scene

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -1,12 +1,8 @@
 extends Node2D
+class_name LevelParent
 
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
-
-func _on_gate_player_entered_gate() -> void:
-	var tween = create_tween()
-	tween.tween_property($Player, "speed", 0, 0.5)
-
 
 func _on_player_shoot_laser(pos, direction) -> void:
 	var laser = laser_scene.instantiate() as Area2D
@@ -21,13 +17,3 @@ func _on_player_throw_grenade(pos, direction) -> void:
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-
-
-func _on_house_player_entered() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1)
-
-
-func _on_house_player_exited() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 2)
