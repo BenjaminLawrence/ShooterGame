65fbc21eeb24da8b33cb7339c746020370da1988
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-03-11 21:17:31 +0800

    Fix pickup amounts

--- globals/globals.gd
+++ globals/globals.gd
@@ -8,20 +8,20 @@ var laser_amount = 20:
 	get:
 		return laser_amount
 	set(value):
-		laser_amount += value 
+		laser_amount = value 
 		laser_amount_change.emit()
 var grenade_amount = 5:
 	get:
 		return grenade_amount
 	set(value):
-		grenade_amount += value
+		grenade_amount = value
 		grenade_amount_change.emit()
 var health = 60:
 	get:
 		return health
 	set(value):
-		if (health + value > 100):
+		if (value > 100):
 			health = 100
 		else:
-			health += value
+			health = value
 		health_change.emit()
