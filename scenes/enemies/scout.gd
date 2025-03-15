e49597ed30db772151f62b54e7e06b023adcbbb4
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-03-15 19:31:04 +0800

    Allow scout to detect nearby player

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -1,4 +1,16 @@
 extends CharacterBody2D
 
+var player_nearby: bool = false
+
 func _process(_delta):
 	look_at(Globals.player_pos)
+	if player_nearby:
+		pass
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	player_nearby = true
+
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	player_nearby = false
