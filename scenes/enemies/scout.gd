2bc87361ee92e955443c54e1d03b5d557f7c9753
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-03-15 19:44:49 +0800

    Add shooting for scout

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -1,16 +1,34 @@
 extends CharacterBody2D
 
 var player_nearby: bool = false
+var can_laser: bool = true
+var second_shot: bool = false
+
+signal laser(pos, direciton)
 
 func _process(_delta):
-	look_at(Globals.player_pos)
 	if player_nearby:
-		pass
+		look_at(Globals.player_pos)
+		if can_laser:
+			var pos: Vector2
+			if second_shot:
+				pos = $LaserSpawnPositions/Marker2D.global_position
+			else:
+				pos = $LaserSpawnPositions/Marker2D2.global_position
+			second_shot = !second_shot
+			var direction: Vector2 = (Globals.player_pos - position).normalized()
+			laser.emit(pos, direction)
+			can_laser = false
+			$LaserCooldown.start()
 
 
-func _on_attack_area_body_entered(body: Node2D) -> void:
+func _on_attack_area_body_entered(_body: Node2D) -> void:
 	player_nearby = true
 
 
-func _on_attack_area_body_exited(body: Node2D) -> void:
+func _on_attack_area_body_exited(_body: Node2D) -> void:
 	player_nearby = false
+
+
+func _on_laser_cooldown_timeout() -> void:
+	can_laser = true
