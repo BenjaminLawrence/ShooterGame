73d290e706b0990a75af9364d38ce183683da3e4
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-03-23 13:59:22 +0800

    Make grenades hit containers and entities

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -1,6 +1,14 @@
 extends RigidBody2D
 
 const speed = 750
+var exploding: bool = false
+
+func _process(_delta: float) -> void:
+	if exploding:
+		for target in get_tree().get_nodes_in_group('Container') + get_tree().get_nodes_in_group('Scout'):\
+			if (target as Node2D).position.distance_to(position) < 500:
+				target.hit()
 
 func explode():
+	exploding = true
 	$AnimationPlayer.play("Explosion")
