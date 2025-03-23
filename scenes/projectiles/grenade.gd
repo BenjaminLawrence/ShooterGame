854f4bcb401578735310cb7f92b5d420809042e1
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-03-23 14:06:03 +0800

    Move hit_status to separate variable

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -8,7 +8,8 @@ func _process(_delta: float) -> void:
 	if exploding:
 		var targets = get_tree().get_nodes_in_group('Container') + get_tree().get_nodes_in_group('Scout')
 		for target in targets:
-			if (target as Node2D).position.distance_to(position) < explosion_range:
+			var in_range = target.global_position.distance_to(position) < explosion_range
+			if in_range:
 				target.hit()
 
 func explode():
