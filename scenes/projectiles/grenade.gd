dbe89550ef0ca577ff840833620456a71d338c8e
BenjaminLawrence <benjamin13lawrence@gmail.com>
2025-03-23 14:00:38 +0800

    Seperate grenade range as a constant

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -1,12 +1,13 @@
 extends RigidBody2D
 
 const speed = 750
+const explosion_range = 500
 var exploding: bool = false
 
 func _process(_delta: float) -> void:
 	if exploding:
 		for target in get_tree().get_nodes_in_group('Container') + get_tree().get_nodes_in_group('Scout'):\
-			if (target as Node2D).position.distance_to(position) < 500:
+			if (target as Node2D).position.distance_to(position) < explosion_range:
 				target.hit()
 
 func explode():
